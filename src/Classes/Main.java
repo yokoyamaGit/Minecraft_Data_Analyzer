dae4bc9fcfdb7e2c61962543993bd09d30eb200e
cotali <alinacotsur@gmail.com>
2022-06-12 15:22:53 +0300

    Description:^)дескрипшн

--- src/Classes/Main.java
+++ src/Classes/Main.java
@@ -3,8 +3,10 @@ package Classes;
 public class Main {
 
     public static void main(String[] args) {
-
+        System.out.println("Hi Putin");
         System.out.println("Hello World! 123");
+        System.out.println("Hi Olega");
+        System.out.println("Hi Liza");
 
     }
 
